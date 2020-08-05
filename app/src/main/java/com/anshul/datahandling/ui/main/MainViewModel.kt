24ac0871356183f77558be4cb40b0f21fc6dccce
Anshul <anshul@particle41.com>
2020-08-05 19:21:51 +0530

    implementing moshi to read data

--- app/src/main/java/com/anshul/datahandling/ui/main/MainViewModel.kt
+++ app/src/main/java/com/anshul/datahandling/ui/main/MainViewModel.kt
@@ -1,19 +1,40 @@
 package com.anshul.datahandling.ui.main
 
 import android.app.Application
+import android.util.JsonReader
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import com.anshul.datahandling.LOG_TAG
+import com.anshul.datahandling.R
+import com.anshul.datahandling.data.Monster
 import com.anshul.datahandling.utilities.FileHelper
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import kotlin.reflect.typeOf
 
 class MainViewModel(app: Application) : AndroidViewModel(app) {
 
+    private val listType = Types.newParameterizedType(
+            List::class.java, Monster::class.java
+    )
+
     init {
+//        val text = FileHelper.getTextFromResources(app, R.raw.monster_data)
         val text = FileHelper.getTextFromAssets(app, "monster_data.json")
-        //val text = FileHelper.getTextFromResources(app, R.raw.monster_data)
+        parseText(text)
+    }
 
-        Log.i(LOG_TAG, text)
+    fun parseText(text: String) {
+        val moshi = Moshi.Builder().build()
+        val adapter: JsonAdapter<List<Monster>> =
+                moshi.adapter(listType)
+        val monsterData = adapter.fromJson(text)
 
+        for (monster in monsterData ?: emptyList()) {
+            Log.i(LOG_TAG,
+                    "${monster.monsterName} (\$${monster.price})")
+        }
     }
 
 }
